ede830bf93b307ebb708cb12bc73c2e8df6755e0
Corinne Marshall <corinnemarshall@Corinnes-iMac.local>
2021-08-23 20:13:01 -0400

    created js functions to load tasks and save to localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,34 @@ const currentDate = moment().format("dddd MMMM Do");
 $("#currentDate").html(currentDate);
 
 const currentHour = moment().format('HH');
+
+//create function to check current time
+function checkTime() {
+    $('textarea').each(function () {
+        if ($(this).attr('id') == currentHour) {
+            $(this).addClass('present');
+        } else if ($(this).attr('id') < currentHour) {
+            $(this).addClass('past');
+        } else if ($(this).attr('id') > currentHour) {
+            $(this).addClass('future');
+        }
+        setInterval(checkTime, (1000 * 60));
+    })
+};
+
+//create function to load tasks
+function loadTasks () {
+    //first check to see if task is from current date, if not clear it from localStorage
+    if (currentHour > 17) {
+        localStorage.clear();
+        window.location.reload;
+    };
+
+    Object.keys(localStorage).forEach((key) => {
+        $('textarea').each(function () {
+            if ($(this).attr('id') == key) {
+                $(this).setInterval(localStorage.getItem(key));
+            }
+        });
+    });
+};
